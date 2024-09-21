81406a823bb880f4f028dabecae9d976cb56aa56
Ali Mohammad <alihmohammad@hotmail.com>
2024-09-21 13:31:48 +0200

    feat: finished includes and index and last index of

--- src/strlib.c
+++ src/strlib.c
@@ -84,4 +84,112 @@ int str_starts_with(char* str, char* startsWith) {
     return matchCount == startsWithLength ? 1 : 0;
 }
 
+int str_includes(char* str, char* includesStr) {
+    int matchCount = 0;
+    int includesStrLength = 0;
+    char* startP = includesStr;
 
+    while (*includesStr)
+    {
+        includesStrLength++;
+        includesStr++;
+    }
+
+    includesStr -= includesStrLength;
+
+    while (*str)
+    {
+        if (*str == *includesStr) {
+            matchCount++;
+            includesStr++;
+            str++;
+
+            if (matchCount == includesStrLength) {
+                return 1;
+            }
+
+            continue;
+        }
+
+        includesStr = startP;
+        matchCount = 0;
+        str++;
+    }
+    
+    return 0;
+}
+
+int str_index_of(char* str, char* indexOfStr) {
+    int matchCount = 0;
+    int indexOfStrLength = 0;
+    char* indexOftrStartP = indexOfStr;
+    char* strStartP = str;
+
+    while (*indexOfStr)
+    {
+        indexOfStrLength++;
+        indexOfStr++;
+    }
+
+    indexOfStr -= indexOfStrLength;
+
+    while (*str)
+    {
+        if (*str == *indexOfStr) {
+            matchCount++;
+
+            if (matchCount == indexOfStrLength) {
+                return (str - strStartP) - indexOfStrLength + 1;
+            }
+
+            indexOfStr++;
+            str++;
+            continue;
+        }
+
+        indexOfStr = indexOftrStartP;
+        matchCount = 0;
+        str++;
+    }
+    
+    return -1;
+}
+
+int str_last_index_of(char* str, char* lastIndexOfStr) {
+    int matchCount = 0;
+    int lastIndexofStrLength = 0;
+    char* lastIndexOfStrP = lastIndexOfStr;
+    char* strStartP = str;
+
+    while (*lastIndexOfStr)
+    {
+        lastIndexofStrLength++;
+        lastIndexOfStr++;
+    }
+
+    while (*str)
+    {
+        str++;
+    }
+
+    while (str >= strStartP)
+    {
+        if (*str == *lastIndexOfStr) {
+            matchCount++;
+
+            if (matchCount == lastIndexofStrLength) {
+                return (str - strStartP);
+            }
+
+            lastIndexOfStr--;
+            str--;
+            continue;
+        }
+
+        lastIndexOfStr = (lastIndexOfStrP + lastIndexofStrLength) - 1;
+        matchCount = 0;
+        str--;
+    }
+    
+    return -1;
+}
--- src/ui/main.c
+++ src/ui/main.c
@@ -4,10 +4,10 @@
 
 int main(int argc, char const *argv[])
 {
-    char str[] = "hello";
-    char str2[] = "ll";
+    char str[] = "hello sir, how are your doing?";
+    char indexOf[] = "your";
 
-    int result = str_starts_with(str, str2);
+    int result = str_index_of(str, indexOf);
 
     printf("%d\n", result);
     
